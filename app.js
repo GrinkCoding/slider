950ecad7e53572f9c5efc950f66aea69e43aac0e
Grink <66211284+GrinkCoding@users.noreply.github.com>
2020-12-22 08:23:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,33 +5,42 @@ const position = document.querySelector(".position-box");
 
 
 const slider = [
-    "https://images.unsplash.com/photo-1576346618381-facf2a17af3d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1045&q=80",
+    "https://images.unsplash.com/photo-1576346618381-facf2a17af3d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
     "https://images.unsplash.com/photo-1451408446993-f6a39d2e667e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1607442436078-10606378d956?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1055&q=80",
-    "https://images.unsplash.com/photo-1605992678821-17fb316eb4a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1073&q=80"
+    "https://images.unsplash.com/photo-1607442436078-10606378d956?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1605992678821-17fb316eb4a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
 ]
 
+// const slider = [
+//     "ressource/photo1.jpg",
+//     "ressource/photo2.jpg",
+//     "ressource/photo3.jpg",
+//     "ressource/photo4.jpg",
+// ]
+
 let numImg = 0;
-imageBox.innerHTML= `<img src=${slider[slider.length-1]} alt="photo_noel">`
+imageBox.innerHTML= `<img src=${slider[numImg]} alt="photo_noel">`
 
 droite.addEventListener("click", function(){
-   imageBox.innerHTML= `<img src=${slider[numImg]} alt="photo_noel">`;
-
+   
     if(numImg !== (slider.length-1)){
         numImg++;
 
     } else{
         numImg=0;
     }
+    imageBox.innerHTML= `<img src=${slider[numImg]} alt="photo_noel">`;
 });
 
 gauche.addEventListener("click", function(){
-   imageBox.innerHTML= `<img src=${slider[numImg]} alt="photo_noel">`;
+   
 
     if(numImg > 0){
         numImg--;
     } else{
         numImg=slider.length-1;
     }
+
+    imageBox.innerHTML= `<img src=${slider[numImg]} alt="photo_noel">`;
 });
 
